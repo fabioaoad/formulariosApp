6863d94160e31f180edee5ad4dc8e7f2a388ebce
Fabio <fabioaoad@gmail.com>
2021-11-07 13:45:27 -0300

    Formularios Reactivos. Seccion 17 - Apartado 7 "Mostrar mensajes de error"

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-basicos',
@@ -6,11 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent{
 
-  constructor() { }
+  //TRABAJO CON FORM GROUP
+  // miFormulario: FormGroup = new FormGroup({
+  //   'nombre':       new FormControl('Geforce fx 5500'),
+  //   'precio':       new FormControl(1500),
+  //   'existencias':  new FormControl(50),
+  // })
 
-  ngOnInit(): void {
+  // TRABAJO CON FORMBUILDER, SE INYECTO EL SERVICIO
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required, Validators.minLength(3)] ],
+    precio: [10, [Validators.required,Validators.min(0)]],
+    existencias: [8, [Validators.required,Validators.min(0)]],
+  })
+
+
+  constructor( private fb: FormBuilder) { }
+
+
+  campoEsValido( campo: string){
+    return this.miFormulario.controls[campo].errors
+          && this.miFormulario.controls[campo].touched;
   }
 
 }
--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ReactiveFormsModule } from "@angular/forms";
+
 import { ReactiveRoutingModule } from './reactive-routing.module';
 import { BasicosComponent } from './basicos/basicos.component';
 import { DinamicosComponent } from './dinamicos/dinamicos.component';
 import { SwitchesComponent } from './switches/switches.component';
+
 
 
 @NgModule({
@@ -15,6 +18,7 @@ import { SwitchesComponent } from './switches/switches.component';
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     ReactiveRoutingModule
   ]
 })
