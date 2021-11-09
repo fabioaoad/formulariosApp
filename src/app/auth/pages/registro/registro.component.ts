395f33528aabbc9717f863f16cf98c91565571ce
Fabio <fabioaoad@gmail.com>
2021-11-09 13:00:54 -0300

    Errores Personalizados para el correo - update - formularios reactivos - seccion 18, apartado 15  - FINAL

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -20,7 +20,23 @@ export class RegistroComponent implements OnInit {
     password2: ['',[Validators.required ]  ]
   },{
     validators: [  this.validatorService.camposIguales('password','password2')]
-    })
+    });
+
+
+    get emailErrorMsg(): string{
+      const errors = this.miFormulario.get('email')?.errors;
+      if ( errors?.required ){
+        return 'Email es obligatorio';
+      }
+      else if ( errors?.pattern ){
+        return 'El valor ingresado no tiene formato de correo';
+      }
+      else if ( errors?.emailTomado ){
+        return 'El email ya fue tomado';
+      }
+    //  console.log('tick');
+      return '';
+    }
 
   constructor( private fb: FormBuilder,
                private  validatorService: ValidatorService,
@@ -42,20 +58,22 @@ export class RegistroComponent implements OnInit {
             && this.miFormulario.get(campo)?.touched;
   }
 
-  emailRequired(){
-    return this.miFormulario.get('email')?.errors?.required
-          && this.miFormulario.get('email')?.touched;
-  }
+  // emailRequired(){
+  //   return this.miFormulario.get('email')?.errors?.required
+  //         && this.miFormulario.get('email')?.touched;
+  // }
+  //
+  // emailFormato(){
+  //   return this.miFormulario.get('email')?.errors?.pattern
+  //     && this.miFormulario.get('email')?.touched;
+  // }
+  //
+  // emailTomado(){
+  //   return this.miFormulario.get('email')?.errors?.emailTomado
+  //     && this.miFormulario.get('email')?.touched;
+  // }
 
-  emailFormato(){
-    return this.miFormulario.get('email')?.errors?.pattern
-      && this.miFormulario.get('email')?.touched;
-  }
 
-  emailTomado(){
-    return this.miFormulario.get('email')?.errors?.emailTomado
-      && this.miFormulario.get('email')?.touched;
-  }
 
   submitFormulario(){
     console.log(this.miFormulario.value);
