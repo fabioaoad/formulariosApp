920520060fd31fc4912d0ec2b2ddf6e4f9ca8692
Fabio <fabioaoad@gmail.com>
2021-11-09 10:08:38 -0300

    Validar email usando una expresión regular - formularios reactivos - seccion 18, apartado 7

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -11,14 +11,20 @@ import {invalid} from "@angular/compiler/src/render3/view/util";
 export class RegistroComponent implements OnInit {
 // TODO: Temporal
   nombreApellidoPatter: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  emailPattern        : string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
 
   miFormulario: FormGroup = this.fb.group({
-    nombre: ['',[Validators.required,Validators.pattern( this.nombreApellidoPatter ) ] ]
+    nombre: ['',[Validators.required,Validators.pattern( this.nombreApellidoPatter ) ] ],
+    email: ['',[Validators.required, Validators.pattern( this.emailPattern ) ] ]
   })
 
   constructor( private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: 'Fabio Aoad',
+      email : 'optimusprime@cybertron.com'
+    });
   }
 
 
