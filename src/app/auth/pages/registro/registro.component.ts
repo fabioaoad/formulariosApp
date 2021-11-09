687a988bba99159348f9902b2fdbf4de681a7f9b
Fabio <fabioaoad@gmail.com>
2021-11-09 10:33:11 -0300

    Validar personalizada del username - formularios reactivos - seccion 18, apartado 8

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {invalid} from "@angular/compiler/src/render3/view/util";
 
 @Component({
@@ -13,17 +13,33 @@ export class RegistroComponent implements OnInit {
   nombreApellidoPatter: string = '([a-zA-Z]+) ([a-zA-Z]+)';
   emailPattern        : string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
 
+  noPuedeStrider( control: FormControl ){
+   // console.log(control.value);
+    const valor: string = control.value?.trim().toLowerCase();
+    //console.log(valor);
+    if( valor === 'strider' ){
+      //return ERROR
+      return {
+        noOptimusPrime: true
+      }
+    }
+    return null;
+  }
+
+
   miFormulario: FormGroup = this.fb.group({
     nombre: ['',[Validators.required,Validators.pattern( this.nombreApellidoPatter ) ] ],
-    email: ['',[Validators.required, Validators.pattern( this.emailPattern ) ] ]
+    email: ['',[Validators.required, Validators.pattern( this.emailPattern ) ] ],
+    username: ['',[Validators.required, this.noPuedeStrider ]  ]
   })
 
   constructor( private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.miFormulario.reset({
-      nombre: 'Fabio Aoad',
-      email : 'optimusprime@cybertron.com'
+      nombre  : 'Fabio Aoad',
+      email   : 'optimusprime@cybertron.com',
+      username: 'bumblebee_autobot'
     });
   }
 
